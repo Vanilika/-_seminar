4210b56870267201151757570cbc0b4b2a3e61bb
valya <vanilik@mail.ru>
2022-12-20 22:53:05 +0300

    Задача 1 Семинар

---
+++ Documents/С#1seminar/Examples/002/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberb = Convert.ToInt32(Console.ReadLine());
+int numbera = Convert.ToInt32(Console.ReadLine());
+
+if(numbera == Convert.ToInt32(Math.Pow(numberb,2)))
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
