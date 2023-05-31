e934a625a109c12b14dfe734cb3eb460e570452e
valya <vanilik@mail.ru>
2023-05-31 08:00:49 +0300

    Python2

---
+++ Documents/itog1chetverty/final_task/task/Program.cs
@@ -0,0 +1,29 @@
+﻿// 4. Написать программу решающую поставленую задачу.
+// из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символам.
+
+string[] existingarray = new string[] {"один", "два", "три", "four", "five", "6", "seven", "8", "nine"};
+string[] result = new string[existingarray.Length];
+int size = 3;
+
+void SizeArray(string[] existingarray, string[] result)
+{
+    int count = 0;
+    for (int i = 0; i < existingarray.Length; i++)
+    {
+        if(existingarray[i].Length <= size)
+        {
+            result[count] = existingarray[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SizeArray(existingarray, result);
+PrintArray(result);
